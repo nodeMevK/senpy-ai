d9822e71cb2d9b52000f39fdc96748b2a0d7c22c
Ghost <nodemev@gmail.com>
2024-10-21 19:00:12 -0400

    still having issues with twitter authentication

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 1. PACKAGES
     1. `pip install anthropic`
-    2. 
+    2. `pip install tweepy`
--- getTwitterInfo.py
+++ getTwitterInfo.py
@@ -1,15 +1,34 @@
 import tweepy
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
+#print(os.getenv("TWITTER_CLIENT"))
 
 auth = tweepy.OAuth1UserHandler(
-    
+    os.getenv("TWITTER_CLIENT"),
+    os.getenv("TWITTER_SECRET"),
+    os.getenv("TWIT_ACCESS_TOKEN"),
+    os.getenv("TWIT_ACCESS_SECRET")
 
 )
 
-twit = tweepy.API()
+client = tweepy.Client(
+    bearer_token=os.getenv("BEARER_TOKEN"),
 
-public_tweets = twit.home_timeline()
+    consumer_key=os.getenv("TWITTER_CLIENT"),
+    consumer_secret=os.getenv("TWITTER_SECRET"),
+    access_token=os.getenv("TWIT_ACCESS_TOKEN"),
+    access_token_secret=os.getenv("TWIT_ACCESS_SECRET")
+
+)
+
+#twit = tweepy.API(auth)
+
+public_tweets = client.get_home_timeline()
+
+#public_tweets = twit.home_timeline()
 
 for tweet in public_tweets:
     print(tweet.text)
