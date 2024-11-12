4dbba5b15fecf83af3395e7608834b0c3d8f510f
Ghost <nodemev@gmail.com>
2024-11-12 17:44:09 -0500

    successfully scraped a tg tweet

--- twitterScraper.py
+++ twitterScraper.py
@@ -1,9 +1,41 @@
 from twitter.scraper import Scraper
 from twitter.util import init_session 
+from twitter.account import Account
 
-scraper = Scraper(session=init_session())
+import os
+from dotenv import load_dotenv, find_dotenv
 
-# get user data
-users = scraper.users(['jack'])
+load_dotenv(find_dotenv())
 
-print(users)
+def getUserId(username):
+    result = scraper.users([username])
+    return result[0]['data']['user']['result']['rest_id']
+
+def getTweets(id):
+    result = scraper.tweets(id)
+    return result
+
+
+cookies = {
+    "ct0": os.getenv("TWIT_COOKIE"),
+    "auth_token": os.getenv("TWIT_AUTH_TOKEN")
+}
+
+'''scraper = Scraper(cookies=)'''
+
+
+#account = Account(cookies=cookies)
+scraper = Scraper(cookies=cookies)
+
+'''tg = scraper.users(['notthreadguy'])
+print(tg)'''
+
+#print(getTweets("notthreadguy"))
+print(getUserId("notthreadguy"))
+#print(getUserId(""))
+
+tweets = scraper.tweets([1426732252768182281],limit=1)
+print(tweets)
+
+#print(account.home_latest_timeline(1))
+
