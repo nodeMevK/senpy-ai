8e30a7edc82a3655770cc2df08b2de05e0fa4d45
Ghost <nodemev@gmail.com>
2024-12-21 08:36:37 -0500

    attempting to seed db

--- database/dbSeed.py
+++ database/dbSeed.py
@@ -2,6 +2,10 @@
 
 
 from models import ShortMemory, LongMemory, User
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.orm import sessionmaker
+import db
 
 
 ''' need to get to this asap'''
+
