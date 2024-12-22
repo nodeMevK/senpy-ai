429f4908192924ba2c286af5b27d2d2696384fec
Ghost <nodemev@gmail.com>
2024-12-22 15:56:47 -0500

    readibility changes

--- database/models.py
+++ database/models.py
@@ -5,17 +5,20 @@ Base = declarative_base()
 
 class ShortMemory(Base):
     __tablename__ = "short_term_memory"
+
     id = Column(Integer, primary_key=True, index=True)
     post = Column(String, index=True)
 
 
 class LongMemory(Base):
     __tablename__ = "long_term_memory"
+
     id = Column(Integer, primary_key=True)
 
 
 class User(Base):
     __tablename__ = "user"
+
     id = Column(Integer, primary_key=True)
     username = Column(String)
     user_id = Column(Integer)
@@ -23,6 +26,7 @@ class User(Base):
 
 class Tweets(Base):
     __tablename__ = "tweets_from_user_scrape"
+
     id = Column(Integer, primary_key=True)
     username = Column(String)
     tweet = Column(String)
@@ -33,6 +37,7 @@ class Tweets(Base):
 
 class TimeLineTweets(Base):
     __tablename__ = "time_line_tweets"
+    
     id = Column(Integer, primary_key=True)
     username = Column(String)
     tweet = Column(String)
