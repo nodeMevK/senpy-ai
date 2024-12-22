43e6d15a8f90c50a96ae09f8849d66a42ab62194
Ghost <nodemev@gmail.com>
2024-12-21 22:33:25 -0500

    cleaned up dbseed and models

--- database/dbSeed.py
+++ database/dbSeed.py
@@ -4,7 +4,6 @@
 from models import ShortMemory, LongMemory, User
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker
-import db
 
 
 ''' need to get to this asap'''
--- database/models.py
+++ database/models.py
@@ -1,27 +1,31 @@
-from database import db
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
-class ShortMemory(db.Model):
-    __tablename__ = 'short_term_memory'
-    id = db.Column(db.Integer, primary_key=True)
+Base = declarative_base()
+
+class ShortMemory(Base):
+    __tablename__ = "short_term_memory"
+    id = Column(Integer, primary_key=True, index=True)
+    post = Column(String, index=True)
 
 
-class LongMemory(db.Model):
+class LongMemory(Base):
     __tablename__ = "long_term_memory"
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
 
-class User(db.Model):
+class User(Base):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
 
-class Tweets(db.Model):
+class Tweets(Base):
     __tablename__ = "tweets_from_user_scrape"
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
 
-class TimeLineTweets(db.Model):
+class TimeLineTweets(Base):
     __tablename__ = "time_line_tweets"
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
 
