7b00f7982d04df90ded4db9bbf09e75b38127fe2
Ghost <nodemev@gmail.com>
2024-12-23 23:27:00 -0500

    update prompts

--- remotes/prompts.py
+++ remotes/prompts.py
@@ -3,12 +3,19 @@ def whats_hot_prompt(tweets_data, context):
     template = """
         Analyze the following posts and text
 
-
         {tweets}
         {text}
+
+
     """
 
     return template.format(tweets=tweets_data, text=context)
 
 
-  
+def respond_to_tweet():
+    return
+
+def respond_aggresively_to_tweet():
+    return
+
+
