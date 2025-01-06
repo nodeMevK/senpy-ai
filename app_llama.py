8b73c0179bc0e218f24b8a7615ee2cbbc638b78c
Ghost <nodemev@gmail.com>
2025-01-06 17:34:37 -0500

    somewhat alive now

--- app_llama.py
+++ app_llama.py
@@ -1,12 +1,25 @@
 import ollama
+from remotes import prompts
+from characters import ch1
 
 
 print("running ")
-response = ollama.chat(model='llama3.2', messages=[
+'''response = ollama.chat(model='llama3.2', messages=[
     
   {
     'role': 'user',
     'content': 'Why is the sky blue?',
+  },
+
+])'''
+
+
+response = ollama.chat(model='llama3.2', messages=[
+    
+  {
+    'role': 'user',
+    #'content': 'Why is the sky blue?',
+    'content': prompts.whats_hot_prompt(prompts.get_example_tweets(), "", ch1.narcissist)
   },
 
 ])
