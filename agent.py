b17ddea7134960ab2e0cb81c6dfe873e38b345c6
Ghost <nodemev@gmail.com>
2025-01-23 23:23:45 -0500

    adding to agent class

--- agent.py
+++ agent.py
@@ -1,4 +1,13 @@
+import ollama
+
 class TwitterAgent:
-    def __init__(self):
+    def __init__(self, system_prompt: str):
+        self.system = ollama.create(
+            model='agent', 
+            from_='llama3.2',
+            parameters={"seed": 42, "temperature": 0},    
+            system=system_prompt,
+            stream=False,
+        )
         pass
         
