f6eebb54ca346be54290b5028e69821e0f8ebd99
Ghost <nodemev@gmail.com>
2025-01-24 00:53:12 -0500

    adding to agent.py

--- agent.py
+++ agent.py
@@ -9,5 +9,22 @@ class TwitterAgent:
             system=system_prompt,
             stream=False,
         )
+
         pass
-        
+
+    def generateResponse(self, prompt):
+        response = ollama.chat(model='agent', messages=[
+            {
+                'role': 'user',
+                #'content': 'Why is the sky blue?',
+                #'content': prompts.whats_hot_prompt(prompts.get_example_tweets(), "", ch1.skizo)
+                'content': prompt
+            },
+        ])
+        pass
+
+    def calculateUserScore(self, users):
+        pass
+
+
+    
