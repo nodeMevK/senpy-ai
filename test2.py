5e5c54e1462acef02d5459d93ea2528d77f490e3
nodeMevK <nodemev@gmail.com>
2025-01-27 17:46:38 -0500

    adding to more tests

--- test2.py
+++ test2.py
@@ -1,14 +1,18 @@
 from agent import TwitterAgent
 from characters import ch1
+from remotes import prompts
 
 
 new_agent = TwitterAgent(ch1.skizo, "example", "llama3.2")
 deepSeek_Agent = TwitterAgent(ch1.skizo, "example_deepseek", "deepseek-r1:1.5b")
 
 new_agent.generateStreamResponse("What do you think about the current state of the crypto trenches?")
-print("/n/n")
+print("\n\n")
 print("deepseek response")
 
 
 deepSeek_Agent.generateStreamResponse("What do you think about the current state of the crypto trenches?")
+print("\n\n")
 
+print("whats hot deepseek\n")
+deepSeek_Agent.generateStreamResponse(prompts.whats_hot_without_character(prompts.get_example_tweets(), ""))
