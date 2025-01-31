c899df0fd46e64b8b550319fcd740111e2e1b33c
nodeMevK <nodemev@gmail.com>
2025-01-30 23:52:06 -0500

    add db import, need to touch up dbsetup

--- app_pipeline.py
+++ app_pipeline.py
@@ -3,6 +3,7 @@
 from twitterScraper import GScraper
 from agent import TwitterAgent
 from characters import ch1
+from database import dbSetup
 
 
 Jaine = TwitterAgent(ch1.skizo, "example_deepseek", "deepseek-r1:1.5b")
