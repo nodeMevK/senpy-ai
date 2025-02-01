a8cda60c99898c783461afce0a0bb2624893506d
nodeMevK <nodemev@gmail.com>
2025-02-01 01:53:16 -0500

    setting up pipeline more

--- app_pipeline.py
+++ app_pipeline.py
@@ -3,14 +3,22 @@
 from twitterScraper import GScraper
 from agent import TwitterAgent
 from characters import ch1
-from database import dbSetup
+#from database import dbSetup
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
 
+cookies = {
+    "ct0": os.getenv("TWIT_COOKIE"),
+    "auth_token": os.getenv("TWIT_AUTH_TOKEN")
+}
 
 Jaine = TwitterAgent(ch1.skizo, "example_deepseek", "deepseek-r1:1.5b")
 
-Jaine.generateStreamResponse
+#Jaine.generateStreamResponse
 
-mrScrape = GScraper()
+
+mrScrape = GScraper(cookies)
 
 
