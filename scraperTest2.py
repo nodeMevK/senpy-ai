382b27e6ab082c3064cf0859ed53d7f855a1be8e
Ghost <nodemev@gmail.com>
2025-02-10 23:58:53 -0500

    important test on using scraper

---
+++ scraperTest2.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from twitterScraper import GScraper
+
+import json
+
+load_dotenv(find_dotenv())
+
+cookies = {
+    "ct0": os.getenv("TWIT_COOKIE"),
+    "auth_token": os.getenv("TWIT_AUTH_TOKEN")
+}
+
+testScraper = GScraper(_cookies=cookies)
+
+'''testId = testScraper.getUserId("notthreadguy")
+tweets = testScraper.getTweets(testId)
+
+newTweets = testScraper.getTweetText(tweets)
+
+final = testScraper.getTweetInfo(newTweets)
+print(len(final))
+for section in final:
+    print(section)'''
+
+#print(testScraper.getTweetText(tweets))
+
+
+#print(testScraper.getTimeLineTweets())
+print("grabbing tweets ...")
+tl = testScraper.getLatestTl(1)
+print("obtained tweets ...")
+
+final = testScraper.getEntries(tl)
+print(len(final))
+
+for section in final:
+    print(section)
+'''print("dumping to file ...")
+with open('test.txt', 'w') as file:
+    json.dump(tl, file)'''
+
+print("finished ...")
