f4822c489b36e7ea6c6532caeec81d4fee3e69d7
Ghost <nodemev@gmail.com>
2025-02-12 23:17:25 -0500

    update memory files

--- memory/accessMemory.py
+++ memory/accessMemory.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-from database import models
+#from database.models import 
 
 def getLongTermMemory():
 
     ''' get data from db '''
+    return
+
+def getShortTermMemory():
+
+    ''' get short term mem from db '''
     return
 
 def formatMemory():
