db86c0ebc896aa87bbdeb4cd831ac5f716e38373
Ghost <nodemev@gmail.com>
2025-02-17 23:34:24 -0500

    added a section to chat with the ai - workng well with stream generation

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 from agent import TwitterAgent
 from characters import ch1
 from remotes import prompts
+import asyncio
 
 print("test running")
 
-new_agent = TwitterAgent(ch1.skizo, "example", "llama3.2", 16)
-deepSeek_Agent = TwitterAgent(ch1.skizo, "example_deepseek", "deepseek-r1:1.5b")
+'''new_agent = TwitterAgent(ch1.skizo, "example", "llama3.2", 16)
+deepSeek_Agent = TwitterAgent(ch1.skizo, "example_deepseek", "deepseek-r1:1.5b")'''
 
-new_agent.generateStreamResponse("What do you think about the current state of the crypto trenches?")
+#new_agent.generateStreamResponse("What do you think about the current state of the crypto trenches?")
 print("\n\n")
 '''print("deepseek response")
 
@@ -17,3 +18,22 @@ print("\n\n")
 
 print("whats hot deepseek\n")'''
 #deepSeek_Agent.generateStreamResponse(prompts.whats_hot_without_character(prompts.get_example_tweets(), ""))
+
+
+#print("Alyssa Response: \n")
+alyssa_agent = TwitterAgent(ch1.alyssa_character, "example2", "llama3.2")
+#alyssa_agent.generateStreamResponse("How do I tell my coworker that they  did a bad job on an assignment?")
+
+while True:
+    try:
+        #print('enter question ...')
+        question = input("\nenter question ... ")
+        #asyncio.run(alyssa_agent.asyncChat(question))
+        alyssa_agent.generateStreamResponse(question)
+    except KeyboardInterrupt:
+        print('\nGoodbye')
+        break
+
+
+    
+        
