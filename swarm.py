290491ae2c8c65903aba79ddc234b3f16f292253
Ghost <nodemev@gmail.com>
2025-02-27 03:05:04 -0500

    update swarm.py

--- swarm.py
+++ swarm.py
@@ -3,34 +3,49 @@ import threading
 import multiprocessing
 
 class Swarm:
-    def __init__(self, _agents: list[Agent]):
+    def __init__(self, _agents: list[Agent], _manager: Agent):
 
         self.agents = _agents
-
-        def coordinatedThinkingThreading(prompt: str):
-            threads = []
-            for agent in self.agents:
-                thread = threading.Thread(target=agent.generateStreamResponse, args=(prompt,))
-                threads.append(thread)
-                thread.start()
-
-            for thread in threads:
-                thread.join()
-
-            return
-
-
-        def coordinatedThinkingProcessing(prompt: str):
-            processes = []
-            for agent in self.agents:
-                process = multiprocessing.Process(target=agent.generateStreamResponse, args=(prompt,))
-                processes.append(process)
-                process.start()
-
-            for process in processes:
-                process.join()
-
-            return 
-        
+        self.managerAgent = _manager
 
         pass
+
+    # function where manager agent decides score of agent reponses
+    # actually might fix this with a pedantic approach 
+    def managerScore(self, agentResponses: list):
+        for response in agentResponses:
+            self.managerAgent.generateStreamResponse("")
+
+        return
+        
+    def coordinatedThinkingThreading(self, prompt: str):
+        threads = []
+        for agent in self.agents:
+            thread = threading.Thread(target=agent.generateStreamResponse, args=(prompt,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        return
+
+
+    def coordinatedThinkingProcessing(self, prompt: str):
+        processes = []
+        for agent in self.agents:
+            process = multiprocessing.Process(target=agent.generateStreamResponse, args=(prompt,))
+            processes.append(process)
+            process.start()
+
+        for process in processes:
+            process.join()
+
+        return 
+    
+    # list agent info 
+    def listAgentInfo():
+        return
+    
+
+        
