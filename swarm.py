acc1d7589e0a5eb7e914e579e1ae4a4838dfc1d4
Ghost <nodemev@gmail.com>
2025-02-26 19:08:16 -0500

    updating swarms

--- swarm.py
+++ swarm.py
@@ -7,10 +7,28 @@ class Swarm:
 
         self.agents = _agents
 
-        def coordinatedThinking(prompt: str):
+        def coordinatedThinkingThreading(prompt: str):
             threads = []
             for agent in self.agents:
                 thread = threading.Thread(target=agent.generateStreamResponse, args=(prompt,))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+
+            return
+
+
+        def coordinatedThinkingProcessing(prompt: str):
+            processes = []
+            for agent in self.agents:
+                process = multiprocessing.Process(target=agent.generateStreamResponse, args=(prompt,))
+                processes.append(process)
+                process.start()
+
+            for process in processes:
+                process.join()
 
             return 
         
